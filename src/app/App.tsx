7d4ed4adece2269634b74dbc16e1156f1d0862d5
Aaron Paredes Cabrera <iron0406@outlook.com>
2022-11-28 21:54:34 -0500

    chore: ordering code

---
+++ src/app/App.tsx
@@ -0,0 +1,21 @@
+/* 
+  PAGES:
+    - HOME
+    - RECIPE
+
+  CONTAINERS:
+    - LAYOUT GENERAL
+    - LAYOUT RECIPE
+
+  COMPONENTS:
+    - BUTTON
+    - CARD
+    - SELECTED RECIPE CELL
+    - CATEGORY CELL
+*/
+
+const App = () => {
+  return <div className="app">App</div>;
+};
+
+export default App;
