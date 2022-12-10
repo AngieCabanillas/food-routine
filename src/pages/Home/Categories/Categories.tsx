3e4f2751ae6a5e5840dc671b21feb2e984dc2e22
Aaron Paredes Cabrera <iron0406@outlook.com>
2022-12-09 19:21:45 -0500

    chore: add axios config

---
+++ src/pages/Home/Categories/Categories.tsx
@@ -0,0 +1,11 @@
+import Container from "../../../ui/Container";
+
+const Categories = () => {
+  return (
+    <Container width="100%" height="100%" backgroundColor="red">
+      Categories
+    </Container>
+  );
+};
+
+export default Categories;
---
+++ src/shared/api/clientAxios.ts
@@ -0,0 +1,31 @@
+import axios from "axios";
+import jwtDecode from "jwt-decode";
+import { API } from "../constant/api";
+import { getAuthToken } from "../storage/auth";
+
+const API_URL = API;
+
+const request = axios.create({ baseURL: API_URL });
+
+request.interceptors.request.use(async (config: any) => {
+  const token = getAuthToken();
+  if (token) {
+    const user = jwtDecode<any>(token);
+    const secondsSinceEpoch = Math.round(Date.now() / 1000);
+    //checking for time expiration of the token
+    if (secondsSinceEpoch > parseInt(user.exp + "")) {
+      localStorage.removeItem("auth_token");
+      window.location.href = "/login";
+      return;
+    }
+  }
+  config.headers["Authorization"] = `Bearer ${token}`;
+  config.headers["Content-Type"] = "application/json";
+  return config;
+});
+
+request.interceptors.response.use(async (response: any) => {
+  return response;
+});
+
+export default request;
