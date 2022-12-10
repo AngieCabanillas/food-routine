e2e7a44df50c9c350dd4f822ac1728a72a14961a
Aaron Paredes Cabrera <iron0406@outlook.com>
2022-12-09 19:48:37 -0500

    chore: add logic to get the categories from API

--- src/pages/Home/Categories/Categories.tsx
+++ src/pages/Home/Categories/Categories.tsx
@@ -1,9 +1,36 @@
+import { useEffect, useState } from "react";
+import { CategorieType } from "../../../shared/models/categories.model";
+import { getCategories } from "../../../shared/services/categoriesServices";
+import Button from "../../../ui/Button";
 import Container from "../../../ui/Container";
 
 const Categories = () => {
+  const [categories, setCategories] = useState<Array<CategorieType>>([]);
+
+  useEffect(() => {
+    const data = getCategories();
+    data.then((data) => {
+      setCategories(data.data.categories);
+    });
+  }, []);
+
   return (
-    <Container width="100%" height="100%" backgroundColor="red">
-      Categories
+    <Container
+      width="100%"
+      height="100%"
+      display="flex"
+      alignItems="center"
+      padding="0 24px"
+      gap="10px"
+      overFlowX="auto"
+    >
+      {categories.map((categorie, index) => {
+        return (
+          <div key={index}>
+            <Button width="150px" title={categorie.strCategory} />
+          </div>
+        );
+      })}
     </Container>
   );
 };
