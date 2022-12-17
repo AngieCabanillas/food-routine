e938ae51736db1413721bcf21b3435d5ed6b3aaf
Aaron Paredes Cabrera <iron0406@outlook.com>
2022-12-16 19:42:17 -0500

    chore: add recipe provider to handle recipes getting from API

---
+++ src/shared/contexts/RecipesProvider.tsx
@@ -0,0 +1,48 @@
+import { createContext, useContext, useState } from "react";
+import { RecipeType } from "../models/recipes.model";
+
+export const RecipesContext = createContext<{
+  recipes: Array<RecipeType>;
+  addRecipe: (recipe: RecipeType) => void;
+  removeRecipe: (id: string) => void;
+} | null>(null);
+
+export const useRecipeContext = () => {
+  const context = useContext(RecipesContext);
+
+  if (context === null) {
+    throw new Error("useRecipeContext must be used within a RecipesContext");
+  }
+
+  return context;
+};
+
+type RecipeProviderProps = {
+  children: React.ReactNode;
+};
+
+export const RecipesProvider: React.FC<RecipeProviderProps> = ({
+  children,
+}) => {
+  const [recipes, setRecipes] = useState<Array<RecipeType>>([]);
+
+  const addRecipe = (recipe: RecipeType) => {
+    setRecipes((prev) => [...prev, recipe]);
+  };
+
+  const removeRecipe = (id: string) => {
+    setRecipes((prev) => prev.filter((recipe) => recipe.idMeal !== id));
+  };
+
+  return (
+    <RecipesContext.Provider
+      value={{
+        recipes,
+        addRecipe,
+        removeRecipe,
+      }}
+    >
+      {children}
+    </RecipesContext.Provider>
+  );
+};
